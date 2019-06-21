075746887f799d582b27395b92b906d403484740
won518 <sewonpark518@gmail.com>
2019-06-20 22:31:04 -0700

    added responsive nav bar. collapsing nav bar for med/small screen

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,29 +1,9 @@
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
 $(document).ready(function(){
   $('.parallax').parallax();
 });
 
-
-// var card1 = '#card1', card2 = '#card2', card3 = '#card3', card4 = '#card4'
-// $(card1 + "," + card2 + "," + card3 + "," + card4).on('click', function () {
-//   this.classList.toggle('is-flipped');
-// });
-
-
-
-
-
-
-
-
-// window.onscroll = function () { myFunction() };
-
-// var navbar = document.getElementById("nav");
-// var sticky = navbar.offsetTop;
-
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
